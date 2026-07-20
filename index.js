41883a748e56da0e0481ba5baf8a130db1c629c2
gerwin-verin <gerwinverin@gmail.com>
2026-07-20 22:38:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const token = urlParams.get('token');
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const token = urlParams.get("token");
 
-    console.log("TOKEN: "+ token);
+  console.log("TOKEN: " + token);
 
-    if (token != null) {
-        if (token == "admin") {
-            $("#txtGuestName").text("Mr. Admin");
-        } else {
-            $("#txtGuestName").text("Mr. Admin TEST");
-        }
+  if (token != null) {
+    if (token == "admin") {
+      $("#txtGuestName").text("Mr. Admin");
+    } else {
+      $("#txtGuestName").text("Mr. Admin TEST");
     }
-})
+  }
+
+  $("#btnOpenInvitation").on("click", function () {
+    window.location.href = "invitation.html";
+  });
+});
